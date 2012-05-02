5fde259215bc2514fcb42199c4eca768c3f4616c
PortgasDAce <saiifii@live.de>
2012-05-02 06:31:05 +0200

    DB/VP: Some Howling Gale updates
    Signed-off-by: PortgasDAce <saiifii@live.de>

--- Instances/The Vortex Pinnacle/3_VP_creature_template.sql
+++ Instances/The Vortex Pinnacle/3_VP_creature_template.sql
@@ -1,2 +1,5 @@
 -- Zephyr Speed
 UPDATE `creature_template` SET `speed_walk`='5', `speed_run`='5' WHERE (`entry`='45991');
+
+-- Howling Gale
+UPDATE creature_template SET unit_flags = 131076, minlevel = 84, maxlevel = 84, faction_A = 16, faction_H = 16 WHERE entry = 45572;
