4f7cf78b0dd4486b49a0677b4c1b4008cf275b1a
PortgasDAce <saiifii@live.de>
2012-05-02 06:19:28 +0200

    DB/VP: Fixed Visual Aura from Howling Gale and Speed from Zephyr
    Signed-off-by: PortgasDAce <saiifii@live.de>

--- Instances/The Vortex Pinnacle/1_VP_addon.sql
+++ Instances/The Vortex Pinnacle/1_VP_addon.sql
@@ -16,3 +16,7 @@ REPLACE INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `
 REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES 
 ('15812193', '15812193', '0', '0', '0', '0', '85740 85734'),
 ('15812192', '15812192', '0', '0', '0', '0', '85740 85734');
+
+-- Howling Gale Visual Aura
+REPLACE INTO `creature_template_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES 
+('45572', '0', '0', '0', '0', '0', '85084');
