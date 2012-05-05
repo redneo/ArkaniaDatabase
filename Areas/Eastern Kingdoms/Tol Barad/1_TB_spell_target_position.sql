39b29632798bc56ad534b4f24dec3c07e63528d8
Portgas D. Ace <saiifii@live.de>
2012-05-05 05:27:47 +0200

    DB/TB: Fixed right Teleport Coordinates to the City Orgrimmar and Stormwind
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Areas/Eastern Kingdoms/Tol Barad/1_TB_spell_target_position.sql
@@ -0,0 +1,10 @@
+-- Portal Tol'Barad
+-- By Saiifii/PortgasDace
+
+REPLACE INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
+
+-- Tol'Barad to Stormwind
+('96178', '0','-8208.89', '428.871', '118.059', '3.269173'),
+
+-- Tol'Barad to Orgrimmar
+('96177', '1', '2048.1101', '-4377.0297', '98.9161', '4.41825');
