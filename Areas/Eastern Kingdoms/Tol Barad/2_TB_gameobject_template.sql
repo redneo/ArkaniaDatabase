535111ec7847260de32368461f649df7d977015e
Portgas D. Ace <saiifii@live.de>
2012-05-05 05:28:36 +0200

    DB/TB: Fixed Mistake from Mikadmin
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Areas/Eastern Kingdoms/Tol Barad/2_TB_gameobject_template.sql
@@ -0,0 +1,8 @@
+-- Portal Tol'Barad
+-- By Saiifii/PortgasDAce
+
+-- Stormwind Portal
+UPDATE `gameobject_template` SET `data0` = '96178' WHERE `gameobject_template`.`entry` =208227;
+
+-- Orgrimmar Portal
+UPDATE `gameobject_template` SET `data0` = '96177' WHERE `gameobject_template`.`entry` =208226;
