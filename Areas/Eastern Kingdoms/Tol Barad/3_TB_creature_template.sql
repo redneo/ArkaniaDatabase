506aba17c00be2313f4d2232a08c13c9163a7802
Portgas D. Ace <saiifii@live.de>
2012-05-05 17:40:21 +0200

    DB/TB: Fixed the Wellson Cannon to enter it, needs only spawn the boots
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Areas/Eastern Kingdoms/Tol Barad/3_TB_creature_template.sql
@@ -0,0 +1,2 @@
+-- Wellson Cannon Temporary Vehicle fix (Temporary Vehicle)
+UPDATE creature_template SET VehicleId = 244 WHERE entry = 48283;
---
+++ Areas/Eastern Kingdoms/Tol Barad/4_TB_npc_spellclick_spells.sql
@@ -0,0 +1,3 @@
+-- Wellson Cannon
+-- To Enter Wellson Cannon
+REPLACE INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES ('48283', '94354', '1', '0');
