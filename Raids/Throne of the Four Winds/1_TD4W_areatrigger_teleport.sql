e02b246a727387c6f1b5158b4ccd3521247bea54
Portgas D. Ace <saiifii@live.de>
2012-05-08 21:27:51 +0200

    DB/Td4W: Fixed the Coordinates for Entrance to Throne of the Four Winds
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Raids/Throne of the Four Winds/1_TD4W_areatrigger_teleport.sql
@@ -0,0 +1,6 @@
+-- Coordinates
+-- By Saiifii / Portgas D. Ace
+
+-- Throne of The Four Winds (Enterance)
+-- The Raid Plattform is spawned more higher
+UPDATE `areatrigger_teleport` SET `target_position_x`='-305.561', `target_position_y`='816.665', `target_position_z`='199.488', `target_orientation`='0.005025' WHERE (`id`='6672');
