2768663e33268794936be0d51ce18028e0047054
Portgas D. Ace <saiifii@live.de>
2012-05-12 15:33:52 +0200

    DK/Quests: Fixed Into the Realm of Shadows
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Quests/Eastern Kingdoms/Scarlet Enclave/9_Into_the_Realm_of_Shadows.sql
@@ -0,0 +1,7 @@
+######################
+#   DK Start Zone    #
+#     by Saiifii     #
+######################
+
+-- To enter the Horse
+UPDATE `npc_spellclick_spells` SET `cast_flags`='1' WHERE (`npc_entry`='28782');
