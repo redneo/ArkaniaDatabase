99ca0102bee5b74230fbac02ec82e6c1b834c26b
Portgas D. Ace <saiifii@live.de>
2012-05-12 15:35:23 +0200

    DK/Quests: The 2 Quests is useable for Horde and Alliance now its fixed
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Quests/Eastern Kingdoms/Scarlet Enclave/11_A_Meeting_with_Fate.sql
@@ -0,0 +1,7 @@
+######################
+#   DK Start Zone    #
+#     by Saiifii     #
+######################
+
+-- Quest is for Alliance and Horde
+UPDATE `quest_template` SET `RequiredRaces`='0' WHERE (`entry`='12755');
