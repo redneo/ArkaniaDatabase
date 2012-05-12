211246d74c7683d698969fa5691a73b1eeb63bfc
Portgas D. Ace <saiifii@live.de>
2012-05-12 15:32:05 +0200

    DB/DK: Set the Random movement to state movement from Scarlet Peasant
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Quests/Eastern Kingdoms/Scarlet Enclave/6_scarlet_peasant.sql
@@ -0,0 +1,7 @@
+######################
+#   DK Start Zone    #
+#     by Saiifii     #
+######################
+
+-- Scarlet Peasant
+UPDATE creature SET movementType = 0, spawndist = 0 WHERE id = 28557;
