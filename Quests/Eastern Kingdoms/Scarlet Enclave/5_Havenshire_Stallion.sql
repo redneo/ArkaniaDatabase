36458881f0749e7cdce685117ef0156740e15228
Portgas D. Ace <saiifii@live.de>
2012-05-12 15:31:18 +0200

    DK/Quests: Fixed the Faction from Havenshire Stallion
    Signed-off-by: Portgas D. Ace <saiifii@live.de>

---
+++ Quests/Eastern Kingdoms/Scarlet Enclave/5_Havenshire_Stallion.sql
@@ -0,0 +1,7 @@
+######################
+#   DK Start Zone    #
+#     by Saiifii     #
+######################
+
+-- Havenshire Stallion
+UPDATE creature_template SET faction_A = 35, faction_H = 35 WHERE entry = 28605;
